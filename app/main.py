180564227ce022703eba98f4c515f98e1c551d0a
Eduardo Rosa <eduardorosatech@gmail.com>
2025-12-24 15:02:47 -0300

    Add root endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
 from app.routers import nutrition
 
-app = FastAPI(title="Nutri AI API")
+app = FastAPI(
+    title="Nutri API",
+    version="1.0.0"
+)
+
+@app.get("/")
+def root():
+    return {
+        "status": "ok",
+        "message": "Nutri API online",
+        "docs": "/docs"
+    }
 
 app.include_router(nutrition.router)
